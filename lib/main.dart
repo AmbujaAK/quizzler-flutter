845d704b30177ccc0082c10a8dc2b65bd93bbe05
ekmbuma <ambuj.a.kumar@ericsson.com>
2020-09-06 10:12:01 +0530

    questions separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -28,36 +30,19 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  // List<String> questions = [
-  //   'You can lead a cow down stairs but not up stairs.',
-  //   'Approximately one quarter of human bones are in the feet.',
-  //   'A slug\'s blood is green.'
-  // ];
-  //
-  // List<bool> answers = [false,true,true];
+  void checkAnswer(bool answerGiven){
+    bool correctAnswer = quizBrain.getAnswer();
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a : false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a : true),
-    Question(q: 'A slug\'s blood is green.', a : true)
-  ];
-
-  int quesNumber = 0;
-
-  void checkAnswer(){
-    bool checkAnswer = questionBank[quesNumber].questionAnswer;
-    if(checkAnswer){
-      setState(() {
-        scoreKeeper.add(Icon(Icons.check, color: Colors.green,),);
-        quesNumber++;
-      });
-    } else {
-      setState(() {
-        scoreKeeper.add(Icon(Icons.close, color: Colors.red,),);
-        quesNumber++;
-      });
-    }
+    setState(() {
+      if(correctAnswer == answerGiven){
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green,),);
+      } else {
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red,),);
+      }
+      quizBrain.nextQuestion();
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -70,7 +55,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[quesNumber % 3].questionText,
+                quizBrain.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -94,7 +79,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                checkAnswer();
+                checkAnswer(true);
               },
             ),
           ),
@@ -112,7 +97,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                checkAnswer();
+                checkAnswer(false);
               },
             ),
           ),
