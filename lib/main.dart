4def475cef5e3136aac9e35fc733fcd13e8d181d
ekmbuma <ambuj.a.kumar@ericsson.com>
2020-09-06 07:44:07 +0530

    questions added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,28 +26,38 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Icon> scoreKeeper = [];
+
+  // List<String> questions = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.'
+  // ];
+  //
+  // List<bool> answers = [false,true,true];
+
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a : false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a : true),
+    Question(q: 'A slug\'s blood is green.', a : true)
   ];
+
+  int quesNumber = 0;
+
+  void checkAnswer(){
+    bool checkAnswer = questionBank[quesNumber].questionAnswer;
+    if(checkAnswer){
+      setState(() {
+        scoreKeeper.add(Icon(Icons.check, color: Colors.green,),);
+        quesNumber++;
+      });
+    } else {
+      setState(() {
+        scoreKeeper.add(Icon(Icons.close, color: Colors.red,),);
+        quesNumber++;
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -59,7 +70,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionBank[quesNumber % 3].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -83,9 +94,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(Icon(Icons.check, color: Colors.green,),);
-                });
+                checkAnswer();
               },
             ),
           ),
@@ -103,12 +112,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                checkAnswer();
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
         Row(
           children: scoreKeeper,
         )
